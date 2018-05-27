6cb7641743a3345ed6d7df023671900359889a3e
Aranganathan <32329046+ranga95@users.noreply.github.com>
2018-05-27 13:18:17 +0530

    Update mp3to4.py

--- mp3to4.py
+++ mp3to4.py
@@ -1,15 +1,35 @@
 from moviepy.editor import *
+import os
 mp4=input("Paste the location of the file")
 mp4 = mp4.replace( '\\', '\\\\')
 mp4=mp4.replace('"','')
-print(mp4)
-print(type(mp4))
-print(mp4)
-video= VideoFileClip(mp4)
-dotnum=mp4.rfind('.')
-mp3converted=mp4[0:dotnum]+'.mp3'
-mp3converted = mp3converted.replace( '\\\\', '\\')
-print(mp3converted)
-print("The location of the converted file is in the same location{}".format(mp3converted))
-# Make the text. Many more options are available.
-video.audio.write_audiofile(mp3converted)
+if mp4.find(".")==True:
+
+    print(mp4)
+    print(type(mp4))
+    print(mp4)
+    video= VideoFileClip(mp4)
+    dotnum=mp4.rfind('.')
+    mp3converted=mp4[0:dotnum]+'.mp3'
+    mp3converted = mp3converted.replace( '\\\\', '\\')
+    print(mp3converted)
+    print("The location of the converted file is in the same location{}".format(mp3converted))
+    # Make the text. Many more options are available.
+    video.audio.write_audiofile(mp3converted)
+else:
+    listmp4=os.listdir(mp4)
+    print(listmp4)
+    for i in listmp4:
+        if i.endswith(".mp3"):
+            pass
+        else:
+            dirmp4=mp4 +"\\"+i
+            print(  dirmp4  )
+            video = VideoFileClip( dirmp4)
+            dotnum = dirmp4.rfind( '.' )
+            mp3converted = dirmp4[0:dotnum] + '.mp3'
+            mp3converted = mp3converted.replace( '\\\\', '\\' )
+            print( mp3converted )
+            print( "The location of the converted file is in the same location{}".format( mp3converted ) )
+            # Make the text. Many more options are available.
+            video.audio.write_audiofile( mp3converted )
